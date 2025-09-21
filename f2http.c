a73882ba206ff4376e1efe8cb5f6e019cd77bcda
oli4vr <oli4vr@gmail.com>
2025-09-21 15:21:54 +0200

    segmented downloads

--- f2http.c
+++ f2http.c
@@ -1,56 +1,119 @@
+/* f2http.c
+   File to HTTP download server
+   by darkQ
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <netinet/in.h>
-#include <sys/socket.h>
+#include <sys/sendfile.h>
 #include <sys/stat.h>
 
-#define BUF_SIZE 4096
+#define BUF_SIZE 262144
+
+void send_response(int client, int fd, off_t filesize, const char *filename, off_t start, off_t end) {
+    char header[1024];
+    int header_len;
+    off_t to_send = end - start + 1;
+
+    header_len = snprintf(header, sizeof(header),
+        "HTTP/1.1 206 Partial Content\r\n"
+        "Content-Type: application/octet-stream\r\n"
+        "Content-Disposition: attachment; filename=\"%s\"\r\n"
+        "Accept-Ranges: bytes\r\n"
+        "Content-Range: bytes %ld-%ld/%ld\r\n"
+        "Content-Length: %ld\r\n"
+        "\r\n",
+        filename, start, end, filesize, to_send);
+
+    write(client, header, header_len);
+    lseek(fd, start, SEEK_SET);
+    off_t sent = 0;
+    while (sent < to_send) {
+        ssize_t n = sendfile(client, fd, NULL, to_send - sent);
+        if (n <= 0) break;
+        sent += n;
+    }
+}
+
+void send_full_response(int client, int fd, off_t filesize, const char *filename) {
+    char header[1024];
+    int header_len;
+
+    header_len = snprintf(header, sizeof(header),
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: application/octet-stream\r\n"
+        "Content-Disposition: attachment; filename=\"%s\"\r\n"
+        "Accept-Ranges: bytes\r\n"
+        "Content-Length: %ld\r\n"
+        "\r\n",
+        filename, filesize);
+
+    write(client, header, header_len);
+    off_t sent = 0;
+    while (sent < filesize) {
+        ssize_t n = sendfile(client, fd, NULL, filesize - sent);
+        if (n <= 0) break;
+        sent += n;
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "%s  by darkQ\n", argv[0]);
-        fprintf(stderr, "Usage: %s <file> <port>\n", argv[0]);
+        fprintf(stderr, "%s\n by darkQ\n\n Syntax: %s <file> <port>\n", argv[0],argv[0]);
         return 1;
     }
 
-    int filefd = open(argv[1], O_RDONLY);
-    if (filefd < 0) { perror("open"); return 1; }
+    const char *filepath = argv[1];
+    int port = atoi(argv[2]);
+    int fd = open(filepath, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
 
     struct stat st;
-    fstat(filefd, &st);
+    if (fstat(fd, &st) < 0) {
+        perror("fstat");
+        return 1;
+    }
+    off_t filesize = st.st_size;
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    const char *filename = strrchr(filepath, '/');
+    filename = filename ? filename + 1 : filepath;
+
+    int server = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(atoi(argv[2]));
-    bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
-    listen(sockfd, 1);
+    addr.sin_port = htons(port);
+
+    bind(server, (struct sockaddr*)&addr, sizeof(addr));
+    listen(server, 5);
 
     while (1) {
-        int client = accept(sockfd, NULL, NULL);
-        char buf[BUF_SIZE];
-        read(client, buf, BUF_SIZE); // Read request (not parsed)
+        int client = accept(server, NULL, NULL);
+        if (client < 0) continue;
 
-        dprintf(client,
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: application/octet-stream\r\n"
-            "Content-Length: %ld\r\n"
-            "Content-Disposition: attachment; filename=\"%s\"\r\n"
-            "\r\n",
-            st.st_size, strrchr(argv[1], '/') ? strrchr(argv[1], '/')+1 : argv[1]);
+        char buf[BUF_SIZE] = {0};
+        read(client, buf, sizeof(buf) - 1);
 
-        lseek(filefd, 0, SEEK_SET);
-        ssize_t n;
-        while ((n = read(filefd, buf, BUF_SIZE)) > 0)
-            write(client, buf, n);
-
+        if (strncmp(buf, "GET ", 4) == 0) {
+            char *range = strstr(buf, "Range: bytes=");
+            if (range) {
+                off_t start = 0, end = filesize - 1;
+                sscanf(range, "Range: bytes=%ld-%ld", &start, &end);
+                if (end >= filesize) end = filesize - 1;
+                if (start > end) start = end;
+                send_response(client, fd, filesize, filename, start, end);
+            } else {
+                send_full_response(client, fd, filesize, filename);
+            }
+        }
         close(client);
     }
-    close(filefd);
-    close(sockfd);
+    close(fd);
     return 0;
 }
